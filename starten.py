d9d298b33f55af8c33760acb76466bf48f4b8dfe
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2025-02-12 20:55:45 +0100

    next

--- starten.py
+++ starten.py
@@ -5,15 +5,15 @@ import geheim
 client = OpenAI(api_key=geheim.key())
 
 invoer = input("Waar wil je een afbeelding over? ")
+for x in range(10):
+    response = client.images.generate(
+    model="dall-e-3",
+    prompt=''''''+invoer+'''''',
+    n=1,
+    size="1792x1024",
+    style="vivid",
+    quality="hd"
+    )
 
-response = client.images.generate(
-  model="dall-e-3",
-  prompt=''''''+invoer+'''''',
-  n=1,
-  size="1792x1024",
-  style="vivid",
-  quality="hd"
-)
-
-print(response)
-webbrowser.open(response.data[0].url)
+    print(response)
+    webbrowser.open(response.data[0].url)
