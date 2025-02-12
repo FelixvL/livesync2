dd00745c17cd86e51ef0cdc9d7f0e088107a6a2c
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2025-02-12 20:38:26 +0100

    next

--- starten.py
+++ starten.py
@@ -9,7 +9,7 @@ completion = client.chat.completions.create(
   model="gpt-4o",
   messages=[{
       "role": "system",
-      "content": ''''''+invoer+''''''
+      "content": '''maak in html een website over:'''+invoer+''''''
     }],
     temperature=0.8,
     max_completion_tokens=5000
@@ -20,7 +20,7 @@ completion = client.chat.completions.create(
 print(completion.choices[0].message.content)
 
 tekst = completion.choices[0].message.content
-bestandsnaam = "voorbeeld.txt"
+bestandsnaam = "voorbeeld.html"
 
 with open(bestandsnaam, "w") as bestand:
     bestand.write(tekst)
