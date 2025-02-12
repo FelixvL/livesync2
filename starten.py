15b39fe579e02c64b8f46d3774152a003019817b
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2025-02-12 19:40:32 +0100

    go

---
+++ starten.py
@@ -0,0 +1,21 @@
+import os
+import openai
+
+import geheim
+
+
+from openai import OpenAI
+client = OpenAI(api_key=geheim.key())
+invoer = input("stel een vraag? ")
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[{
+      "role": "system",
+      "content": ''''''+invoer+''''''
+    }],
+    temperature=0.8,
+    max_completion_tokens=1024
+)
+
+print(completion)
+# print(completion.choices[0].message.content)
