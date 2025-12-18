410e306304864ee463773d800abb00008bed391d
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2025-12-18 10:33:09 +0100

    next

--- starten.py
+++ starten.py
@@ -1,1 +1,13 @@
-print("hoi!")
+from openai import OpenAI
+
+api_key="abc"
+
+
+client = OpenAI(api_key=api_key)
+
+response = client.responses.create(
+    model="gpt-4.1-mini",
+    input="Leg uit wat prompt engineering is in 2 zinnen."
+)
+
+print(response.output_text)
